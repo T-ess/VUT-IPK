d09f355001493ca24897b60bb63401226211c2a2
Tereza Burianova <40072706+T-ess@users.noreply.github.com>
2021-03-30 00:23:29 +0200

    Added the getall functionality

--- fileget.py
+++ fileget.py
@@ -8,6 +8,7 @@ import argparse
 from urllib.parse import urlparse
 import re
 import sys
+import os
 
 def argument_parse():
     """Parses the parameters: fileget -n NAMESERVER -f SURL
@@ -18,22 +19,46 @@ def argument_parse():
     parser = argparse.ArgumentParser(description='Parse the fileget command.')
     parser.add_argument('-n', required=True)
     parser.add_argument('-f', required=True)
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
+    except:
+        sys.exit("Error parsing the arguments. Usage: fileget -n NAMESERVER -f SURL")
     #* parse SURL 
-    surl = urlparse(args.f)
+    try:
+        surl = urlparse(args.f)
+    except:
+        sys.exit("Invalid SURL.")
     if surl.scheme != 'fsp':
-        exit("Use the fsp protocol.")
+        sys.exit("Use the fsp protocol.")
     #* parse NAMESERVER - ip and port
     nameserver = args.n.split(':', 1)
     nameserver[1] = int(nameserver[1]) # port to integer
 
-def fsp(server, file, path):
-    s_fsp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def file_from_path(path):
+    filename = path.split("/")
+    filename = filename[-1]
+    return filename
 
+def fsp(server, file, path, all = False):
+    #* getall - rename file if already exists
+    if all:
+        i = 1
+        while os.path.exists(file):
+            file = f'copy{i}-' + file
+            i += 1
+
+    s_fsp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_fsp.settimeout(600)
     #* connect and send GET request
-    s_fsp.connect((fileserver[0], fileserver[1])) #TODO: try expect
-    get_request = f"GET {surl_path} FSP/1.0\r\nHostname: {surl.hostname}\r\nAgent: xburia28\r\n\r\n"
-    s_fsp.sendall(bytes(get_request, 'utf-8'))
+    try:
+        s_fsp.connect((fileserver[0], fileserver[1]))
+    except:
+        sys.exit("Error connecting to the server.")
+    get_request = f"GET {path} FSP/1.0\r\nHostname: {server}\r\nAgent: xburia28\r\n\r\n"
+    try:
+        s_fsp.sendall(bytes(get_request, 'utf-8'))
+    except:
+        sys.exit("Error sending the GET request.")
 
     getanswer = ''
     filelength = 0
@@ -43,74 +68,105 @@ def fsp(server, file, path):
 
     #* receive and save results
     while True:
-
-        getanswer = s_fsp.recv(512)
-
+        try:
+            getanswer = s_fsp.recv(512)
+        except:
+            sys.exit("Error receiving the data.")
         if parseHeader: #* first run of the loop
-
             #* check server answer for success/error
-            #TODO: exception pokud prijde prazdna odpoved - no answer from server a exit
             if getanswer == b'':
-                exit("uh oh")
+                s_fsp.close()
+                sys.exit("Unexpected response from the server.")
             header = getanswer.splitlines()
             if header[0] != b'FSP/1.0 Success':
                 get_err = True
-
             #* save file length from header
             filelength = int(re.search(rb'\d+', header[1]).group())
-
             #* remove the header
-            getanswer = re.split(rb'(?:\r\n|\r(?!\n)|(?<!\r)\n){2}', getanswer)
+            getanswer = re.split(rb'(?:\r\n){2}', getanswer)
             getanswer = bytes(getanswer[1])
-
             #* create the file
             if not get_err:
-                f = open(surl_file, "wb") #TODO: error handling
+                try:
+                    f = open(file, "wb")
+                except:
+                    s_fsp.close()
+                    sys.exit("Cannot create the local file.")
 
         #* if the whole answer was received
         if (not getanswer) and (not parseHeader):
             if get_err:
                 s_fsp.close()
-                exit(msg_err)   
+                sys.exit(msg_err)
             else:
                 f.close()
+                if os.stat(file).st_size != filelength:
+                    if os.path.exists(file):
+                        os.remove(file)
+                    sys.exit("The connection was unexpectedly terminanted. The file was removed.")
             break
-
         #* add received text to file/error message
         if get_err:
             getanswer = getanswer.decode("utf-8")
             msg_err += getanswer
         else:
-            f.write(getanswer)
-        
+            try:
+                f.write(getanswer)
+            except:
+                s_fsp.close()
+                if os.path.exists(file):
+                    os.remove(file)
+                sys.exit("Unable to write into the local file. The file was removed.")
+        #* set first run to finished
         parseHeader = False
 
     s_fsp.close()
+
 
 argument_parse()
 
 ####* Name Service Protocol #####
 
 s_nsp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s_nsp.sendto(bytes(f"WHEREIS {surl.hostname}\r\n", 'utf-8'), (nameserver[0], nameserver[1])) #TODO: try expect
-fileserver = s_nsp.recv(32).decode('utf-8')
-#TODO: timeout
+try:
+    s_nsp.sendto(bytes(f"WHEREIS {surl.hostname}\r\n", 'utf-8'), (nameserver[0], nameserver[1]))
+    s_nsp.settimeout(5)
+    fileserver = s_nsp.recv(32).decode('utf-8')
+except:
+    sys.exit("Error in UDP connection. Check the IP address.")
 s_nsp.close()
 
-if fileserver == '':
-    exit("NSP server error")
-if fileserver[0:3] == 'ERR':
-    exit("NSP request error")
+if (fileserver == '') or (fileserver[0:3] == 'ERR'):
+    sys.exit("Unable to get the file server IP address.")
 
 fileserver = fileserver[3:].split(":", 1) # ip address + port
 fileserver[1] = int(fileserver[1]) # port
 
 #* get file path and name
+getall = False
 surl_path = '.' + surl.path
-surl_file = surl_path.split("/")
-surl_file = surl_file[-1]
+surl_file = file_from_path(surl_path)
 if surl_path == "./" + surl_file:
     surl_path = surl_file
 
+if surl_file == '*':
+    if surl_file != surl_path:
+        sys.exit("Invalid SURL. GETALL ('*') can only be called from the root folder.")
+    getall = True
 
-fsp(surl.hostname, surl_file, surl_path)
+if getall:
+    fsp(surl.hostname, "index", "index")
+    try:
+        f_index = open('index', 'r')
+    except:
+        sys.exit('Unable to open the "index" file.')
+    ready_files = []
+    for line in f_index:
+        line_file = file_from_path(line.strip())
+        #TODO: nefunguje pro sobory s copy uz pred jmenem
+        if os.path.exists(line_file) and (line_file not in ready_files):
+            os.remove(line_file)
+        ready_files.append(line_file)
+        fsp(surl.hostname, line_file, line.strip(), True)
+else:
+    fsp(surl.hostname, surl_file, surl_path)
